59a79175f9d63a23861eefec54162d2c5053c894
KAUSHALDIXITDUMMYy <kaushal.s.dixit5@gmail.com>
2025-07-22 00:21:38 +0530

    Colour fixxed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cyberpunk-black': '#18181b',
+        'cyberpunk-violet': '#6d28d9',
+        'cyberpunk-pink': '#ec4899',
+        'cyberpunk-blue': '#38bdf8',
+        'cyberpunk-yellow': '#fde047',
+        'cyberpunk-green': '#22d3ee',
+        'cyberpunk-red': '#f43f5e',
+      },
+    },
   },
   plugins: [],
 };
